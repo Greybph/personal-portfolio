080dfc74ca14e2945aa862816f04803d79a5ed45
andrew fauth <andrewlfauth@gmail.com>
2022-02-07 14:51:25 -0800

    Part 4 tut

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        title: "Andrew Fauth"
     },
     plugins: [
-
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
     ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const HomePage = () => {
   return (
     <Layout pageTitle='Home'>
       <p>Im following the tutorial.</p>
+      <StaticImage
+        alt="picture of a dog"
+        src="../images/fav.png"
+      />
     </Layout>
   )
 }
